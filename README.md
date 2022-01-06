d95d5ae3788a6424c427b6d903a9db1434dc0737
Lilia <lilia.mehamli@protonmail.com>
2022-01-06 01:59:32 +0000

    [add] metadata extracting script

---
+++ README.md
@@ -0,0 +1,37 @@
+# Offline Geotagged-Image Locater
+
+A webapp that locates any geotagged image file on a local, offline map within the constraint of the Algerian area. It uses the folium library.
+
+## Project Setup
+
+Cloning the project
+
+```
+$ git clone https://github.com/cr105ph1nx/offline-geotagged-image-locater
+$ cd offline-geotagged-image-locater
+```
+
+Setting up a virtual environment
+
+```
+$ pip install virtualenv
+$ virtualenv env
+```
+
+Activating environment on Linux
+
+```
+$ source env/bin/activate
+```
+
+Installing requirements
+
+```
+$ pip install -r requirments.txt
+```
+
+Running the script
+
+```
+$ python geotagging.py path/to/image.jpeg
+```
---
+++ geotagging.py
@@ -0,0 +1,49 @@
+import exifread
+import json
+import requests
+from sys import argv
+
+
+def extractData(img):
+    f = open(img, 'rb')
+    tags = exifread.process_file(f)
+
+    # d = map(str(tags['GPS GPSLatitude']).split(),int)
+    d = str(tags['GPS GPSLatitude'])[1:-1].split(',')[:-1]
+    north_south = -1 if (str(tags['GPS GPSLatitudeRef']) == "S") else 1
+
+    d2 = str(tags['GPS GPSLongitude'])[1:-1].split(',')[:-1]
+    east_west = -1 if (str(tags['GPS GPSLatitudeRef']) == "W") else 1
+
+    for i, item in enumerate(d):
+        d[i] = float(item)
+
+    for i, item in enumerate(d2):
+        d2[i] = float(item)
+
+    lat = north_south*(d[0] + d[1]/60)
+    lon = east_west*(d2[0] + d2[1]/60)
+    # print (d, degrees)
+    return tags, lat, lon
+
+
+def saveData(tags, lat, lon):
+    # Appending the following tags to the output file:
+    accepted_tags = ["Image Orientation", "Image ResolutionUnit", "Image DateTime",
+                     "EXIF ExifImageWidth", "EXIF ExifImageLength", "EXIF LensModel"]
+    for tag in tags.keys():
+        for accepted_tag in accepted_tags:
+            if tag.startswith(accepted_tag):
+                with open(f'exifdata/test.txt', "a") as file:
+                    file.write("%s:%s\n" % (tag, tags[tag]))
+
+    # Append latitude, longitude
+    with open(f'exifdata/test.txt', "a") as file:
+        file.write(f'GPS Latitude: {lat}\n')
+        file.write(f'GPS Longitude: {lon}\n')
+
+
+if __name__ == '__main__':
+    img = str(argv[1])
+    tags, lat, lon = extractData(img)
+    saveData(tags, lat, lon)
